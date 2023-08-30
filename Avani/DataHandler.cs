13b802be6bf26d273eb3d88569cbf0a2b5e475ba
Avani Jain <92999202+inavaj@users.noreply.github.com>
2023-08-30 15:20:27 +0530

    Add files via upload

---
+++ Avani/DataHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataHandler : MonoBehaviour
+{
+    public GameObject food;
+    private static DataHandler instance;
+
+
+    public static  DataHandler Instance
+    {
+        get
+        {
+           if (instance == null)
+           {
+                instance = FindObjectOfType<DataHandler>();
+           }
+           return instance;
+        }
+    }
+
+}
